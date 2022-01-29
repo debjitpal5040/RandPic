9ff5fd5f92ab43964101c0a052f39d3bc79e8885
Debjit Pal <debjitpal5040@gmail.com>
2022-01-29 16:01:43 +0530

    Initial Commit

---
+++ RandPic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandPic
+//
+//  Created by Debjit Pal on 29/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
