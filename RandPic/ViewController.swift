bf9936141bcf51e884f1b0405e6df120bd1493fb
Debjit Pal <debjitpal5040@gmail.com>
2022-01-29 22:37:34 +0530

    Update

--- RandPic/ViewController.swift
+++ RandPic/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -18,34 +18,15 @@ class ViewController: UIViewController {
     
     private let button: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .white
-        button.setTitle("Random Picture", for: .normal)
+        button.backgroundColor = .systemYellow
+        button.setTitle("Next Picture", for: .normal)
         button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 20
+        button.clipsToBounds = true
         return button
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-        view.addSubview(imageView)
-        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-        imageView.center = view.center
-        
-        view.addSubview(button)
-        getRandomPicture()
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-    }
-    
-    @objc func didTapButton(){
-        getRandomPicture()
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        button.frame = CGRect(x: 30, 
-                              y:view.frame.size.height-150-view.safeAreaInsets.bottom,
-                              width: view.frame.size.width-60,
-                              height: 55)
-    }
+    let colors: [UIColor] = [.systemPink, .systemTeal, .systemRed, .systemGreen, .systemOrange, .systemCyan, .systemBlue, .systemGray, .systemPurple]
     
     func getRandomPicture(){
         let urlString = "https://source.unsplash.com/random/600x600"
@@ -55,7 +36,31 @@ class ViewController: UIViewController {
         }
         imageView.image = UIImage(data: data)
     }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 390, height: 350)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+        getRandomPicture()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton(){
+        getRandomPicture()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 100,
+                              y:view.frame.size.height-130-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-200,
+                              height: 55)
+    }
 
 }
 
